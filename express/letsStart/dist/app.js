18428e2ad9b581b4d87b2e550c2a33a107ba8eb6
hyojaeKwon <khj010909@gm.gist.ac.kr>
2022-09-12 21:13:12 +0900

    feat: add middle ware

--- express/letsStart/dist/app.js
+++ express/letsStart/dist/app.js
@@ -1,12 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
+var app_model_1 = require("./app.model");
 var app = express();
 var port = 8000;
-var data = [1, 2, 3, 4];
+app.use(function (req, res, next) {
+    console.log("this is logging middle ware");
+    console.log(req.rawHeaders[1]);
+    next();
+});
 app.get("/", function (req, res) {
     console.log(req);
-    res.send({ data: data });
+    res.send({ cats: app_model_1.Cat });
 });
 app.listen(port, function () {
     console.log("Example app listening at http://localhost:" + port);
--- express/letsStart/src/app.ts
+++ express/letsStart/src/app.ts
@@ -1,15 +1,21 @@
 import * as express from "express";
+import { Cat, CatType } from "./app.model";
 
 const app: express.Express = express();
 // instance of express
 
 const port: number = 8000;
 
-const data = [1, 2, 3, 4];
+app.use((req, res, next) => {
+  console.log("this is logging middle ware");
+  console.log(req.rawHeaders[1]);
+  next();
+});
+// 미들웨어는 가장 상단에 위치해야함.
 
 app.get("/", (req: express.Request, res: express.Response) => {
   console.log(req);
-  res.send({ data });
+  res.send({ cats: Cat });
   //   res.send(req.body);
 });
 
