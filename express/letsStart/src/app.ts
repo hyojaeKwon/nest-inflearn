f9b0bb5e0936eb3bcf8662608dbd5e1a84273ed9
hyojaeKwon <khj010909@gm.gist.ac.kr>
2022-09-12 21:01:10 +0900

    feat: create cat type and mock data

--- express/letsStart/src/app.ts
+++ express/letsStart/src/app.ts
@@ -1,1 +1,19 @@
-console.log("hello world!!");
+import * as express from "express";
+
+const app: express.Express = express();
+// instance of express
+
+const port: number = 8000;
+
+const data = [1, 2, 3, 4];
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  console.log(req);
+  res.send({ data });
+  //   res.send(req.body);
+});
+
+// opening a server
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
